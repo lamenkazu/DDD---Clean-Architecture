408da209befb2b161fed29b28361e8737efc401d
Lamenkazu <333.erick@gmail.com>
2024-03-05 15:37:01 -0300

    ⚙️ Dependências externas

--- src/domain/services/answer-question.test.ts
+++ src/domain/services/answer-question.test.ts
@@ -1,10 +1,18 @@
 import { expect, test } from "vitest";
 import { AnswerQuestionService } from "./answer-question";
+import { AnswerRepository } from "../repositories/answers-repository";
+import { Answer } from "../entities/answer";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionService();
+const fakeAnswerRepository: AnswerRepository = {
+  create: async function (answer: Answer) {
+    return;
+  },
+};
 
-  const answer = answerQuestion.execute({
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionService(fakeAnswerRepository);
+
+  const answer = await answerQuestion.execute({
     content: "Nova resposta",
     instructorId: "1",
     questionId: "1",
