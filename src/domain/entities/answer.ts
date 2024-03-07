b057ec5031983538b57c09e1df80b2436633bef1
Lamenkazu <333.erick@gmail.com>
2024-03-07 14:46:04 -0300

    👨‍💻 Mapeando mais propriedades das perguntas e respostas

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -2,9 +2,11 @@ import { Entity } from "../../core/entities/entity";
 import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 
 interface AnswerProps {
-  content: string;
   authorId: UniqueEntityId;
   questionId: UniqueEntityId;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Answer extends Entity<AnswerProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,10 +3,13 @@ import { Entity } from "../../core/entities/entity";
 import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
-  title: string;
-  slug: Slug;
-  content: string;
   authorId: UniqueEntityId;
+  bestAnswerId?: UniqueEntityId;
+  title: string;
+  content: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
