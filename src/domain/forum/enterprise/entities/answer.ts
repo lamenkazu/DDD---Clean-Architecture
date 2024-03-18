3f1d235f05a956ae0fa18a1854a68d0628552627
Lamenkazu <333.erick@gmail.com>
2024-03-17 22:13:54 -0300

    ✨ Pub: Answer Created Event

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -2,6 +2,8 @@ import { Optional } from "@/core/@types/optional";
 import { Entity } from "@/core/entities/entity";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { AnswerAttachmentList } from "./answer-attachment-list";
+import { AggregateRoot } from "@/core/entities/aggregate-root";
+import { AnswerCreatedEvent } from "../events/answer-created-event";
 
 export interface AnswerProps {
   authorId: UniqueEntityId;
@@ -12,7 +14,7 @@ export interface AnswerProps {
   updatedAt?: Date;
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   static create(
     props: Optional<AnswerProps, "createdAt" | "attachments">,
     id?: UniqueEntityId
@@ -25,6 +27,12 @@ export class Answer extends Entity<AnswerProps> {
       },
       id
     );
+
+    const isNewAnswer = !id; // Só é uma nova resposta se não tiver recebido o id como parametro.
+
+    if (isNewAnswer) {
+      answer.addDomainEvent(new AnswerCreatedEvent(answer));
+    }
 
     return answer;
   }
